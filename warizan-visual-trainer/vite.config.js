e2978ae4af57919695e126787da1f009de47c2e9
2dice_K <rjhxx419@gmail.com>
2025-03-07 17:51:19 +0900

    test

--- warizan-visual-trainer/vite.config.js
+++ warizan-visual-trainer/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   //base: '/warizan-visual-trainer/warizan-visual-trainer/'
   //outDir: '../../public'
     build: {
-        base: './', //相対パスでビルド
+        base: './',
         outDir: '../docs', //出力場所の指定
     },
 })
